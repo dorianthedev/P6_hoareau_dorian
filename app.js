be2b1b73b67a79c524d4789fd03edfa3a45b4f26
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-02-23 21:18:20 +0100

    import morgan et mongoose

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
 //importation de express
 const express = require('express');
 
+//importation de moragn (logger http)
+const morgan = require('morgan')
+
+//importation fichiers db pour connexion base de donnée mongoDB avec package mongoose
+const mongoose = require('./db/db')
+
 // pour créer une application express
 const app = express();
 
+// logger les requetes et les reponses
+app.use(morgan('dev'));
+
+
+
+// test si le server réponds
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
 
 // exportation de app.js pour y accéder depuis un autre fichier
 module.exports = app;
