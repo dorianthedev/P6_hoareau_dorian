a1e9c55e48d933f52158dc785cd1e4f2e04c33e8
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-02-24 11:21:48 +0100

    add CORS

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 
 //importation de moragn (logger http)
 const morgan = require('morgan');
+// logger les requetes et les reponses
+app.use(morgan('dev'));
 
 //importation fichiers db pour connexion base de donnée mongoDB avec package mongoose
 const mongoose = require('./db/db');
@@ -10,9 +12,15 @@ const mongoose = require('./db/db');
 // pour créer une application express
 const app = express();
 
-// logger les requetes et les reponses
-app.use(morgan('dev'));
-
+// gérer les problèmes de CORS ( cross-origin request sharing ) = sécurity
+// autorisation à tout le monde d'accès à la web api, pas de rejet
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // d'accéder à notre API depuis n'importe quelle origine ( '*' ) ;
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  // d'envoyer des requêtes avec les méthodes mentionnées
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
 
 // test si le server réponds
