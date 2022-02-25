eff30da9c3a9568b7f5ac8a407221c96d87641f0
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-02-25 09:24:42 +0100

    hash password send save succes

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,24 @@
+// impoortation de bcrypt pour hash le password
+const bcrypt = require('bcrypt');
 // importation du models de la base de donnée User.js
-const User = require('../models/User')
+const User = require('../models/User');
 
 
 //signup pour enregistrer un noucel user dans la base de donnée
 // exports pour exporter dnas la routes/user.js
+// hash du password avant de l'envoyer dans la db
+
 exports.signup = (req, res, next) => {
-    
+        bcrypt.hash(req.body.password, 10) // 10 = salt = nombre de fois hash algorithme
+        .then(hash => {
             const user = new User({
             email: req.body.email,
-            password: req.body.password
+            password: hash
             });
             user.save()
             .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
             .catch(error => res.status(400).json({ error }));
-        
+        })
+        .catch(error => res.status(500).json({ error }));
 };
 
