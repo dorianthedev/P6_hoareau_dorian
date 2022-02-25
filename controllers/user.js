d52f971829c3e8e1e368259b147fcf4b674a57c4
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-02-25 09:07:39 +0100

    before hash send save succes user

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,18 @@
 // importation du models de la base de donnée User.js
 const User = require('../models/User')
-console.log(User);
+
 
 //signup pour enregistrer un noucel user dans la base de donnée
 // exports pour exporter dnas la routes/user.js
 exports.signup = (req, res, next) => {
-    console.log("contenu");
-    console.log(req.body);
-    next()
+    
+            const user = new User({
+            email: req.body.email,
+            password: req.body.password
+            });
+            user.save()
+            .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+            .catch(error => res.status(400).json({ error }));
+        
 };
 
