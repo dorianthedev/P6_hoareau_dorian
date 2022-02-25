117069f083c2255a5d9e47e620495d20bdff7790
dorianthedev <93143334+dorianthedev@users.noreply.github.com>
2022-02-25 15:19:30 +0100

    cryptojs email succes

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,27 @@
 // impoortation de bcrypt pour hash le password
 const bcrypt = require('bcrypt');
+// importation de cryptojs pour chiffrer le mail
+const cryptojs = require('crypto-js')
 // importation du models de la base de donnée User.js
 const User = require('../models/User');
+// importation pour utiliser les variables d'environnement
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
 
 
 //signup pour enregistrer un noucel user dans la base de donnée
 // exports pour exporter dnas la routes/user.js
-// hash du password avant de l'envoyer dans la db
 
 exports.signup = (req, res, next) => {
+        // chiffrer l'email avant de l'envoyer de la base de donnée
+        const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, `${process.env.CRYPTOJS_EMAIL}`).toString();
+
+        // hash du password avant de l'envoyer dans la db
         bcrypt.hash(req.body.password, 10) // 10 = salt = nombre de fois hash algorithme
         .then(hash => {
             const user = new User({
-            email: req.body.email,
+            email: emailCryptoJs,
             password: hash
             });
             user.save()
